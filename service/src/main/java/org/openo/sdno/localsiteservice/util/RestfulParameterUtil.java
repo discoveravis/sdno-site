a77b0f755ad865e4183d63e6a0224107ade50ab0
angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
2017-02-04 11:15:46 +0800

    Add RestParameter operation class
    Add RestParameter operation class,provide API to fill data in rest parameters.
    
    SDNO-1142
    
    Signed-off-by: angilecoder <zhaoyangzhao.zhaoyang@huawei.com>
    Change-Id: Ie193027e64b94d481cbfb29a5825fd926e5f20c3

---
+++ service/src/main/java/org/openo/sdno/localsiteservice/util/RestfulParameterUtil.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdno.localsiteservice.util;
+
+import org.openo.baseservice.roa.util.restclient.RestfulParametes;
+import org.openo.sdno.overlayvpn.security.authentication.HttpContext;
+
+/**
+ * Util class of RestParameters.<br>
+ * 
+ * @author
+ * @version SDNO 0.5 2017-1-11
+ */
+public class RestfulParameterUtil {
+
+    /**
+     * Set Content Type.<br>
+     * 
+     * @param restfulParameters RestfulParametes Object
+     * @param contentType Content type
+     * @since SDNO 0.5
+     */
+    public static void setContentType(RestfulParametes restfulParameters, String contentType) {
+        restfulParameters.put(HttpContext.CONTENT_TYPE_HEADER, contentType);
+    }
+
+    /**
+     * Set Default Content Type.<br>
+     * 
+     * @param restfulParameters RestfulParametes Object
+     * @since SDNO 0.5
+     */
+    public static void setContentType(RestfulParametes restfulParameters) {
+        restfulParameters.put(HttpContext.CONTENT_TYPE_HEADER, "application/json;charset=UTF-8");
+    }
+
+    /**
+     * Set Controller Uuid.<br>
+     * 
+     * @param restfulParameters RestfulParametes Object
+     * @param ctrlUuid controller uuid
+     * @since SDNO 0.5
+     */
+    public static void setControllerUuid(RestfulParametes restfulParameters, String ctrlUuid) {
+        restfulParameters.putHttpContextHeader("X-Driver-Parameter", "extSysID=" + ctrlUuid);
+    }
+}
